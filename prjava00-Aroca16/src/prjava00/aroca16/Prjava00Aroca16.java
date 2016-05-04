f70886e2df27382d76f98b37fae4b729f9b3dfb5
ubu2 <ubu2@m-ubu2>
2016-05-04 20:01:33 +0200

    Versió 0.1 del projecte prjava00-Aroca16

---
+++ prjava00-Aroca16/src/prjava00/aroca16/Prjava00Aroca16.java
@@ -0,0 +1,17 @@
+
+
+package prjava00.aroca16;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Prjava00Aroca16 {
+
+    
+    
+    public static void main(String[] args) {
+		System.out.println("Prjava00-Aroca16");
+		System.out.println("Versió 0.1 del projecte prjava00-Aroca16");
+	}
+    }
+    
+
