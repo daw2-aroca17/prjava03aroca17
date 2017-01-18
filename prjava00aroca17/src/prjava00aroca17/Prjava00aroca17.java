fa575d88a213bb9f5579bcf336b0efed02223301
ubuanna <marc.aroca.postigo@gmail.com>
2017-01-18 18:06:47 +0100

    versio 0.1  del projecte prjava00aroca17

---
+++ prjava00aroca17/src/prjava00aroca17/Prjava00aroca17.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjava00aroca17;
+
+/**
+ *
+ * @author ubu2
+ */
+public class Prjava00aroca17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Projecte de Aroca17");
+        System.out.println("Versio 0.1 del projecte prjava00aroca17");
+    }
+    
+}
